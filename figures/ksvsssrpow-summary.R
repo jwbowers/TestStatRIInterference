0341263a1ad45368d002ebabb6967e6fd403622e
jwbowers <jake@jakebowers.org>
2015-12-04 09:56:15 -0500

    Small summary of the KS versus SSR no interference comparison

--- figures/ksvsssrpow-summary.R
+++ figures/ksvsssrpow-summary.R
@@ -1,5 +1,13 @@
 # A file to summarize the results of the KS versus SSR power comparison for no interference but difference models of effects and outcomes
 
-load("simulations/ksvsssrpow-results.rda")
+if(length(grep("TestStatRIInterference$",getwd(),ignore.case=TRUE))==0){setwd("..")}
+if(length(grep("TestStatRIInterference",.libPaths(),ignore.case=TRUE))==0){.libPaths(".libraries") }
+source("simulation/setup.R")
+load("simulation/ksvsssrpow-results.rda")
+ALTS<-list(tau=sort(unique(c(0,seq(-5,5,length=100)))))
+
+apply(powComparison,2,summary)
+
+t(powComparison[powComparison[,"resultsCOANormKS"]<1,])
 
 
