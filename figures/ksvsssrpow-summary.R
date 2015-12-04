cad5b4afd28193eae2d6d4e2403464b09b50864e
Jake Bowers <jake@jakebowers.org>
2015-12-04 08:46:23 -0600

    Started a file to summarize the results of the KS versus SSR no interference comparison

---
+++ figures/ksvsssrpow-summary.R
@@ -0,0 +1,5 @@
+# A file to summarize the results of the KS versus SSR power comparison for no interference but difference models of effects and outcomes
+
+load("simulations/ksvsssrpow-results.rda")
+
+
