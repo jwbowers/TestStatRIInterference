3625c14e3839d26911252e274573cfc84cfe0581
jwbowers <jake@jakebowers.org>
2015-12-15 14:29:55 -0500

    Figures and summaries of the simple KS versus SSR simulation

--- figures/ksvsssrpow-summary.R
+++ figures/ksvsssrpow-summary.R
@@ -16,3 +16,14 @@ t(powCOM[powCOM[,"resultsCOMNormKS"]<1,])
 ## SSR is slightly more power than KS for COA with Normal outcomes
 ## KS is more powerful than SSR for COA with Zif outcomes
 ## SSR is more powerful that KS for COM with Normal outcomes and less power than KS for Zif outcomes
+
+pdf(file="KSVSSSRSimplePowplot.pdf",width=8,height=4)
+par(mfrow=c(1,2),oma=rep(0,4),mgp=c(1.5,.5,0),pty="s",mar=c(3,3,2,0))
+matplot(x=ALTS$COA$tau,powCOA,type="l",lty=c(1,2,1,2),col=c(1,1,2,2),
+	main="Constant Additive Model",xlab="Hypotheses (Truth=0)",ylab="pr(p<=.05)")
+legend(x="bottomleft",legend=substr(colnames(powCOA),11,50),lty=c(1,2,1,2),col=c(1,1,2,2),bty="n")
+matplot(x=ALTS$COM$tau,powCOM,type="l",lty=c(1,2,1,2),col=c(1,1,2,2),
+	main="Constant Multiplicative Model", xlab="Hypotheses (Truth=1)",ylab="pr(p<=.05)")
+legend(x="bottomleft",legend=substr(colnames(powCOM),11,50),lty=c(1,2,1,2),col=c(1,1,2,2),bty="n")
+dev.off()
+
